9f0fbe20b66571b40bbcff6387300194c50f9151
Your Name <you@example.com>
2023-02-09 00:30:18 +0100

    numrpy

---
+++ scripts/number.py
@@ -0,0 +1,17 @@
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+num3 = float(input('Enter the third number: '))
+num4 = float(input('Enter the fourth number: '))
+num5 = float(input('Enter the fifth number: '))
+
+
+# num1 = 512
+# num2 = 282
+# num3 = 47
+# num4 = 48
+# num5 = 5
+
+
+answer = ( num1 - num2 ) / ( (num3*num4) + num5 )
+
+print(f'The answer is {answer}')
