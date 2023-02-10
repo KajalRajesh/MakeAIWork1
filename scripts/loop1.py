1cf1af53417d8c599205a859cee414614210a7d6
Your Name <you@example.com>
2023-02-10 14:13:50 +0100

    practice programs

---
+++ scripts/loop1.py
@@ -0,0 +1,10 @@
+print("please enter a number and press ENTER")
+ #Define the number eleven​
+number = int(input('Enter a number : '))
+ 
+while number > 0:
+     number = number - 3
+if number == 0:
+    print('The number is divisible by 3')
+else:
+    print('The number is not divisible')
