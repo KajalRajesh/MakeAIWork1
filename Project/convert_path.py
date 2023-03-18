b410619e4736dbd542da126525afd03f21a139d0
Your Name <you@example.com>
2023-03-18 23:07:40 +0100

    project_gezondheidzorg

--- Project/convert_path.py
+++ Project/convert_path.py
@@ -2,9 +2,11 @@
 
 import os
 import sys
+#!/usr/bin/env bash
 
 def main():
-    osPath = str(sys.argv[1])
+    osPath = str(sys.argv[0
+                          ])
     print(os.path.abspath(osPath))
 
 if __name__ == "__main__":
